59fd7180dfb1c81981251d32af9f163a0702b9fe
Osman Özden <45053867+osmanozden@users.noreply.github.com>
2021-02-07 11:18:56 +0300

    Add files via upload

---
+++ web_api.py
@@ -0,0 +1,36 @@
+import requests 
+from bs4 import BeautifulSoup as bs 
+
+kullanici_bilgi = {
+    'UserName' : 'kazimhas96',
+    'Password' : 'hXbtvxUNVOs',
+}
+headers = {
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.104 Safari/537.36'
+} 
+def oturum():
+    with requests.session() as s1 :
+
+        url = 'https://www.itemsatis.com/index.php'
+        
+        r = s1.get(url ,headers = headers)
+        
+        soup = bs(r.content ,'html5lib')
+        
+        kullanici_bilgi['csrf_token'] = soup.find("input" , attrs={'name' :  'csrf_token'})['value']
+        
+        #rint(kullanici_bilgi)
+        
+        url = 'https://www.itemsatis.com/api/login.html'
+        
+        giris = s1.post(url,data=kullanici_bilgi,headers=headers)
+        
+        #print(giris.text)
+        
+        url = 'https://www.itemsatis.com/favori-ilanlarim.html'
+        
+        r = s1.get(url , headers = headers)
+        
+        return r.status_code
+
+print(oturum())
